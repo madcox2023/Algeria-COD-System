77633bf740d2cadf939d050ff5d4abda4974a70c
madcox2023 <145722120+madcox2023@users.noreply.github.com>
2026-07-14 20:25:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
+/**
+ * Algeria COD System
+ * app.js
+ * Version 1.0.0
+ */
 
+import { CONFIG } from "./config.js";
+
+import {
+    loadWilayas,
+    loadShippingPrices,
+    populateWilayas
+} from "./shipping.js";
+
+import {
+    calculateOrderTotal,
+    formatPrice
+} from "./calculator.js";
+
+import {
+    validateOrder
+} from "./validator.js";
+
+import {
+    submitOrder
+} from "./api.js";
+
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+
+    console.log("Algeria COD System Started");
+
+}
