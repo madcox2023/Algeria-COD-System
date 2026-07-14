80ef5d45881357bf6826ef880567e9b7f4795270
madcox2023 <145722120+madcox2023@users.noreply.github.com>
2026-07-14 19:39:19 +0100

    Add configuration constants for Algeria COD System

--- js/config.js
+++ js/config.js
@@ -1,1 +1,25 @@
+/**
+ * Algeria COD System
+ * Version: 1.0.0
+ * Configuration
+ */
 
+export const CONFIG = Object.freeze({
+
+    API_URL: "https://script.google.com/macros/s/AKfycbxXmBGz1b0PgCwqeytI1R1uFbo6s4dFX5-CQHZo5gn9Qf3QgUd5yVNmvm-nTs7wwoMv/exec",
+
+    CURRENCY: "دج",
+
+    DEFAULT_LANGUAGE: "ar",
+
+    DEFAULT_COUNTRY: "DZ",
+
+    DEFAULT_QUANTITY: 1,
+
+    DEFAULT_SHIPPING: "home",
+
+    REQUEST_TIMEOUT: 15000,
+
+    VERSION: "1.0.0"
+
+});
